0042ad4ac187f4324525e55904cab4e3ade8b79b
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-21 20:15:37 +0200

    ending

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ console.log(bulb)
 
 const OnBtn = document.getElementById ("accendere")
 console.log(OnBtn)
+
+/* SECONDO STEP:
+Aggiungo un evento click al bottone e creo dunque la funzione in modo che la
+lampadina si accenda */
+
+OnBtn.addEventListener ('click', function(){
+// utilizzo l'immagine della lampadina e la cambio da spenta ad accesa
+bulb.src = "img/yellow_lamp.png"
+
+})
