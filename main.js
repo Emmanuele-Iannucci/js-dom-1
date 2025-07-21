818fad02b23728ae61e040f5197ea7551871affe
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-21 19:49:13 +0200

    select_elements_OnBtn

--- main.js
+++ main.js
@@ -4,3 +4,5 @@ Seleziono gli elementi da utilizzare */
 const bulb = document.getElementById ("lampadina")
 console.log(bulb)
 
+const OnBtn = document.getElementById ("accendere")
+console.log(OnBtn)
